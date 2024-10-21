3c882444659b7b28e8559db10c1df62c2613f7e1
Ryan Haley <me@ryanchaley.com>
2024-10-21 14:46:12 -0400

    feat: Add `DumpRequest` and `DumpResponse` functions
    This commit adds two wrappers around the stdlib `httputil.DumpRequest`
    and `httputil.DumpResponse` functions that always dump the
    request/response body respectively. This is only intended as a
    convenience as this is the behavior I want every time I use those
    functions.

---
+++ httputil.go
@@ -0,0 +1,34 @@
+// Package httputil provides basic utilities for working with and inspecting
+// HTTP requests and responses.
+package httputil
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+// // DumpRequest prints r to stderr including headers and the request body. The
+// request is reset after printing so it's safe to reuse. If the request can't
+// be dumped, an error is emitted to stderr instead.
+func DumpRequest(r *http.Request) {
+	bytes, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dump request failed: %v\n", err)
+		return
+	}
+	fmt.Fprintln(os.Stderr, string(bytes))
+}
+
+// DumpResponse prints resp to stderr including the headers and body. The
+// response is reset after printing so it's safe to reuse. If the response can't
+// be dumped, an error is emitted to stderr instead.
+func DumpResponse(resp *http.Response) {
+	bytes, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dump response failed: %v\n", err)
+		return
+	}
+	fmt.Fprintln(os.Stderr, string(bytes))
+}
